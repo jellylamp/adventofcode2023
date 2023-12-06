37f4f835582d839c177b3f83c5cde0c2dfc5e87d
Caroline Galecki <Caroline@macbook-air.lan>
2023-12-05 22:49:48 -0500

    remove unneeded seed class now ive seen part 2

--- src/05-seed-planting.ts
+++ src/05-seed-planting.ts
@@ -1,89 +1,3 @@
-class Seed {
-    get seedId(): number {
-        return this._seedId;
-    }
-
-    set seedId(value: number) {
-        this._seedId = value;
-    }
-
-    get soilId(): number {
-        return this._soilId;
-    }
-
-    set soilId(value: number) {
-        this._soilId = value;
-    }
-
-    get fertilizerId(): number {
-        return this._fertilizerId;
-    }
-
-    set fertilizerId(value: number) {
-        this._fertilizerId = value;
-    }
-
-    get waterId(): number {
-        return this._waterId;
-    }
-
-    set waterId(value: number) {
-        this._waterId = value;
-    }
-
-    get lightId(): number {
-        return this._lightId;
-    }
-
-    set lightId(value: number) {
-        this._lightId = value;
-    }
-
-    get tempId(): number {
-        return this._tempId;
-    }
-
-    set tempId(value: number) {
-        this._tempId = value;
-    }
-
-    get humidityId(): number {
-        return this._humidityId;
-    }
-
-    set humidityId(value: number) {
-        this._humidityId = value;
-    }
-
-    get locationId(): number {
-        return this._locationId;
-    }
-
-    set locationId(value: number) {
-        this._locationId = value;
-    }
-
-    private _seedId: number;
-    private _soilId: number;
-    private _fertilizerId: number;
-    private _waterId: number;
-    private _lightId: number;
-    private _tempId: number;
-    private _humidityId: number;
-    private _locationId: number;
-
-    constructor(seedId, soilId, fertilizerId, waterId, lightId, tempId, humidityId, locationId) {
-        this._seedId = seedId;
-        this.soilId = soilId;
-        this.fertilizerId = fertilizerId;
-        this.waterId = waterId;
-        this.lightId = lightId;
-        this.tempId = tempId;
-        this.humidityId = humidityId;
-        this.locationId = locationId;
-    }
-}
-
 class Coordinates {
     private source: number;
     private dest: number;
@@ -111,14 +25,7 @@ export class SeedPlanting {
     set lowestLocation(value: number) {
         this._lowestLocation = value;
     }
-    get seedIds(): any[] {
-        return this._seedIds;
-    }
 
-    set seedIds(value: any[]) {
-        this._seedIds = value;
-    }
-    private _seedIds = [];
     private _lowestLocation = -1;
 
     constructor(input: string) {
@@ -148,8 +55,6 @@ export class SeedPlanting {
             const tempId = this.getSeedInfo(lightToTemperatureList, lightId);
             const humidityId = this.getSeedInfo(temperatureToHumidityList, tempId);
             const locationId = this.getSeedInfo(humidityToLocationList, humidityId);
-
-            this.seedIds.push(new Seed(parseInt(seedId), soilId, fertilizerId, waterId, lightId, tempId, humidityId, locationId));
 
             if (this._lowestLocation === -1 || this.lowestLocation > locationId) {
                 this.lowestLocation = locationId;
