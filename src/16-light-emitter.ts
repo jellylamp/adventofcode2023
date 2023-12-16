ed854035aca819a8df8cabeda5d44d3f1eea9539
Caroline Galecki <Caroline@macbook-air.lan>
2023-12-16 10:51:29 -0500

    Woo part a.

--- src/16-light-emitter.ts
+++ src/16-light-emitter.ts
@@ -1,3 +1,10 @@
+
+interface NextMove {
+  nextRow: number,
+  nextCol: number,
+  nextDir: string
+}
+
 export class LightEmitter {
   get visited(): Set<string> {
     return this._visited;
@@ -24,106 +31,119 @@ export class LightEmitter {
   // }
 
     traverseMazeBFS(startRow, startCol, direction) {
-    let queue = [[startRow, startCol, 0, []]]; // [row, col, pathLen, path]
+    let queue = [[startRow, startCol, direction, []]]; // [row, col, pathLen, path]
     const visited = new Set();
 
     while (queue.length > 0) {
-      const [row, col, pathLen, path] = queue.shift();
+      const [row, col, direction, path] = queue.shift();
 
       if (visited.has(this.getKey(row, col))) {
         continue;
       }
 
-      visited.add(this.getKey(row, col));
+      // for the bfs we can about revisiting direction; for the overall grid we dont
+      visited.add(this.getDirectionalKey(row, col, direction));
+      this.visited.add(this.getKey(row, col));
 
-      const neighbors = this.getOrthogonalNeighbors(row, col);
+      const nextMoves = this.moveBeam(row, col, direction);
 
-      for (const [nextRow, nextCol] of neighbors) {
-        if (!visited.has(this.getKey(nextRow, nextCol))) {
-          // Add path length
-          const nextPathLen = pathLen + 1;
-          const nextSymbol = this.grid[nextRow][nextCol];
-          const nextPath = path.concat([{ symbol: nextSymbol, coordinates: [nextRow, nextCol] }]);
-          queue.push([nextRow, nextCol, nextPathLen, nextPath]);
+      nextMoves.forEach(nextMove => {
+        // if undefined we went outside the bounds of the grid, stop.
+        if (nextMove !== undefined) {
+          if (!visited.has(this.getDirectionalKey(nextMove.nextRow, nextMove.nextCol, nextMove.nextDir))) {
+            const nextSymbol = this.grid[nextMove.nextRow][nextMove.nextCol];
+            const nextPath = path.concat([{
+              symbol: nextSymbol,
+              coordinates: [nextMove.nextRow, nextMove.nextCol],
+              direction: nextMove.nextDir
+            }]);
+            queue.push([nextMove.nextRow, nextMove.nextCol, nextMove.nextDir, nextPath]);
+          }
         }
-      }
+      });
     }
   }
 
+  getDirectionalKey(row, col, dir) {
+    return `${row}-${col}-${dir}`;
+  }
+
   getKey(row, col) {
-    return `${row}-${col}`
+    return `${row}-${col}`;
   }
 
   moveBeam(row, col, currentDirection) {
     const currentSymbol = this.grid[row][col];
+    const nextMove = [];
 
     if (currentSymbol === '.') {
       // keep going in current direction
-      this.moveBeanInDirection(row, col, currentDirection);
+      nextMove.push(this.moveBeamInDirection(row, col, currentDirection));
     } else if (currentSymbol === '/') {
       if (currentDirection === 'N') {
-        this.moveBeanInDirection(row, col, 'E');
+        nextMove.push(this.moveBeamInDirection(row, col, 'E'));
       } else if (currentDirection === 'E') {
-        this.moveBeanInDirection(row, col, 'N');
+        nextMove.push(this.moveBeamInDirection(row, col, 'N'));
       } else if (currentDirection === 'S') {
-        this.moveBeanInDirection(row, col, 'W');
+        nextMove.push(this.moveBeamInDirection(row, col, 'W'));
       } else if (currentDirection === 'W') {
-        this.moveBeanInDirection(row, col, 'S');
+        nextMove.push(this.moveBeamInDirection(row, col, 'S'));
       }
     } else if (currentSymbol === '\\') {
       if (currentDirection === 'N') {
-        this.moveBeanInDirection(row, col, 'W');
+        nextMove.push(this.moveBeamInDirection(row, col, 'W'));
       } else if (currentDirection === 'E') {
-        this.moveBeanInDirection(row, col, 'S');
+        nextMove.push(this.moveBeamInDirection(row, col, 'S'));
       } else if (currentDirection === 'S') {
-        this.moveBeanInDirection(row, col, 'E');
+        nextMove.push(this.moveBeamInDirection(row, col, 'E'));
       } else if (currentDirection === 'W') {
-        this.moveBeanInDirection(row, col, 'N');
+        nextMove.push(this.moveBeamInDirection(row, col, 'N'));
       }
     } else if (currentSymbol === '|') {
       // if hit the pointy end, keep going, if hit the flat end split
       if (currentDirection === 'N') {
-        this.moveBeanInDirection(row, col, currentDirection);
+        nextMove.push(this.moveBeamInDirection(row, col, currentDirection));
       } else if (currentDirection === 'E') {
-        this.moveBeanInDirection(row, col, 'N');
-        this.moveBeanInDirection(row, col, 'S');
+        nextMove.push(this.moveBeamInDirection(row, col, 'N'));
+        nextMove.push(this.moveBeamInDirection(row, col, 'S'));
       } else if (currentDirection === 'S') {
-        this.moveBeanInDirection(row, col, currentDirection);
+        nextMove.push(this.moveBeamInDirection(row, col, currentDirection));
       } else if (currentDirection === 'W') {
-        this.moveBeanInDirection(row, col, 'N');
-        this.moveBeanInDirection(row, col, 'S');
+        nextMove.push(this.moveBeamInDirection(row, col, 'N'));
+        nextMove.push(this.moveBeamInDirection(row, col, 'S'));
       }
     } else if (currentSymbol === '-') {
       if (currentDirection === 'N') {
-        this.moveBeanInDirection(row, col, 'E');
-        this.moveBeanInDirection(row, col, 'W');
+        nextMove.push(this.moveBeamInDirection(row, col, 'E'));
+        nextMove.push(this.moveBeamInDirection(row, col, 'W'));
       } else if (currentDirection === 'E') {
-        this.moveBeanInDirection(row, col, currentDirection);
+        nextMove.push(this.moveBeamInDirection(row, col, currentDirection));
       } else if (currentDirection === 'S') {
-        this.moveBeanInDirection(row, col, 'E');
-        this.moveBeanInDirection(row, col, 'W');
+        nextMove.push(this.moveBeamInDirection(row, col, 'E'));
+        nextMove.push(this.moveBeamInDirection(row, col, 'W'));
       } else if (currentDirection === 'W') {
-        this.moveBeanInDirection(row, col, currentDirection);
+        nextMove.push(this.moveBeamInDirection(row, col, currentDirection));
       }
     }
+    return nextMove;
   }
 
-  moveBeanInDirection(row, col, direction) {
+  moveBeamInDirection(row, col, direction): NextMove {
     if (direction === 'N') {
-      if (row - 1 > 0) {
-        this.traverseMazeBFS(row - 1, col, 'N');
+      if (row - 1 >= 0) {
+        return { nextRow: row - 1, nextCol: col, nextDir: 'N' };
       }
     } else if (direction === 'E') {
       if (col + 1 < this.grid.length) {
-        this.traverseMazeBFS(row, col + 1, 'E');
+        return { nextRow: row, nextCol: col + 1, nextDir: 'E' };
       }
     } else if (direction === 'W') {
-      if (col - 1 > 0) {
-        this.traverseMazeBFS(row, col - 1, 'W');
+      if (col - 1 >= 0) {
+        return { nextRow: row, nextCol: col - 1, nextDir: 'W' };
       }
     } else {
       if (row + 1 < this.grid.length) {
-        this.traverseMazeBFS(row + 1, col, 'S');
+        return { nextRow: row + 1, nextCol: col, nextDir: 'S' };
       }
     }
   }
